677edb399384b4716c5c8b97b61e55a05bd6e15d
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-10-11 15:33:49 -0700

    updates-3

--- cltepp.cpp
+++ cltepp.cpp
@@ -8,6 +8,25 @@
 
 // argc is number of arguments
 // argc is number of args, argv is array of args
+void inputs() {
+    noecho(); // Disable echoing of input characters
+    keypad(stdscr, TRUE); // Enable extended keyboard input
+
+    while (true) {
+        int ch = getch(); // Get a character from the keyboard
+
+        if (ch == KEY_UP) { // Check if the up arrow key was pressed
+            // Your code to execute when the up arrow key is pressed
+            mvprintw(LINES - 1, 0, "Up arrow key pressed!");
+            refresh(); // Update the screen
+        } else {
+            // Handle other keys or exit the loop if needed
+            break;
+        }
+    }
+
+}
+
 int main(int argc, char** argv) {
 
     if (argc < 2) {
